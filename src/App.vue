872818a1b92ab4afb49b71377ee73204450583b4
lig8 <lig842@163.com>
2025-04-12 17:26:28 +0800

    test code for components

--- src/App.vue
+++ src/App.vue
@@ -1,126 +1,15 @@
 <script setup>
-import {ref, watch } from "vue";
+  import myButton from "./components/button.vue";
 
-function add(newVal, oldVal) {
-  console.log('newVal: '+ newVal, 'oldVal: '+ oldVal);
-}
-
-function del(index){
-  list.value.splice(index, 1);
-}
-
-const str = ref('');
-
-const list = ref([
-  {
-    isCompleted: true,
-    text: '吃饭'
-  },
-  {
-    isCompleted: false,
-    text: '睡觉'
-  },
-  {
-    isCompleted: false,
-    text: '打豆豆'
+  function add(str){
+    console.log(str)
   }
-]);
-
-watch(str,add);
 </script>
 
 <template>
-  <div class="todo-App">
-
-    <!--  title  -->
-    <div class = "title"> XXX的todo-App </div>
-
-    <!--  add todo  -->
-    <div class="todo-form">
-      <input v-model="str" class="todo-input" type="text" placeholder="add a todo">
-      <div @click="add" class="todo-button">add todo</div>
-    </div>
-
-    <!--  items  -->
-    <div v-for='(item,index) in list' :class="[item.isCompleted ? 'item completed' : 'item']">
-      <div>
-        <input v-model="item.isCompleted" type="checkbox" />
-        <span class="name"> {{ item.text}} </span>
-      </div>
-      <div @click="del(index)" class="del"> del </div>
-    </div>
-
-  </div>
+  <myButton text = "你好" @msg="add"> </myButton>
+  <myButton text= '我好'> </myButton>
+  <myButton  text= '大家好'> </myButton>
 </template>
 
-<style>
-.completed {
-  text-decoration: line-through;
-  opacity: 0.4;
-}
-.del{
-  color: red;
-  cursor: pointer;
-}
-
-.item {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  box-sizing: border-box;
-  width: 80%;
-  height: 50px;
-  margin: 8px auto;
-  padding: 16px;
-  border-radius: 20px;
-  box-shadow:  rgba(149, 157, 165, 0.2) 0px 8px 20px;
-}
-.todo-form{
-  display: flex;
-  marging-top: 20px;
-  margin-left: 30px;
-}
-
-.todo-input{
-  padding-left: 15px;
-  margin-bottom: 20px;
-  width: 60%;
-  height: 50px;
-  border: 1px solid #dfe1e5;
-  outline: none;
-  border-radius: 20px 0 0 20px;
-}
-
-.todo-button{
-  width: 100px;
-  height: 52px;
-  border-radius: 0 20px 20px 0;
-  text-align: center;
-  line-height: 52px;
-  color: #fff;
-  background: linear-gradient(to right, rgb(113,65,168), rgba(44,114,251,1));
-  cursor: pointer;
-  user-select: none;
-}
-
-body {
-  margin: 10px;
-  min-height: 100vh;
-  background: linear-gradient(to right, rgb(113,65,168), rgba(44,114,251,1));
-}
-.todo-App{
-  padding-top: 30px;
-  box-sizing: border-box;
-  width: 98%;
-  margin-top: 40px;
-  margin-left: 1%;
-  height: 500px;
-  background: white;
-  border-radius: 5%;
-}
-.title{
-  font-size: 30px;
-  font-weight: 700;
-  text-align: center;
-}
-</style>
+<style scoped></style>
---
+++ src/components/button.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { defineProps, defineEmits } from 'vue'
+  const props = defineProps(['text'])
+  const emit = defineEmits(['msg'])
+
+  function send(text) {
+    emit('msg',"hello world")
+  }
+</script>
+
+<template>
+  <div class="button" @click="send"> {{props.text}} </div>
+</template>
+
+<style scoped>
+.button{
+  margin: 100px 0 0 200px;
+  width: 100px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  background-color: antiquewhite;
+  border-radius: 10px;
+  cursor: pointer;
+  user-select: none;
+}
+</style>
